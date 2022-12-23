1626d410ddfe04d320b785e174a5dba455cb2ec9
Jorge <jorge.poma@unl.edu.ec>
2022-12-23 16:05:06 -0500

    fix 1.0

---
+++ src/api/publicacions/controllers/publicacions.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  publicacions controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::publicacions.publicacions');
---
+++ src/api/publicacions/routes/publicacions.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * publicacions router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::publicacions.publicacions');
